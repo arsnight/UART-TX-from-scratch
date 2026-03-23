d9d7b05d538898a62f09ba0b7895a630a471ba1f
arsnight <adityars.ars1@gmail.com>
2026-03-23 23:24:00 +0530

    Updated uart_tx.v, with completed code

--- src/uart_tx.v
+++ src/uart_tx.v
@@ -1,25 +1,41 @@
+`timescale 1ns / 1ps
+
 module UART_TX(
     input clk,
     input [7:0] parallel_in,
     input start_tx,
-    output reg [7:0] data_stored,
-    output reg prev_state,
-    output reg serial_out
+    output serial_out
     );
-    
-    wire tick;
-    
-    baud_rate_gen baud_rate(
-        .clk(clk),
-        .tick(tick)
-        );
+
+    reg [7:0] data_stored;
+    reg prev_state;
+    reg edge_detect = 0;
     
     always @(posedge clk) begin
         if (prev_state == 1'b0 && start_tx == 1'b1) begin
             data_stored <= parallel_in;
+            edge_detect <= 1;
         end
         
+        else begin
+            edge_detect <= 0;
+        end
+                
         prev_state <= start_tx;
     end
     
+    wire tick;    
+    baud_rate_gen baud_rate(
+        .clk(clk),
+        .tick(tick)
+        );
+
+    uart_tx_fsm fsm_init(
+        .clk(clk),
+        .data_stored(data_stored),
+        .tick(tick),
+        .edge_detect(edge_detect),
+        .serial_out(serial_out)
+        );
+            
 endmodule
